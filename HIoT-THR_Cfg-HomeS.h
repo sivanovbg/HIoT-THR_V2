b026fd65a480c3e913df16e1227bb6b2eef81db9
Simeon Lap <simeon.ivanov@gmail.com>
2020-05-18 10:05:09 +0300

    Header file changed

--- HIoT-THR_Cfg-HomeS.h
+++ HIoT-THR_Cfg-HomeS.h
@@ -1,7 +1,7 @@
 // Main definitions for the HobbyIoT Sensor node Type 1 THR
 
 #define OWN_ADDRESS 0x9996    // 802.15.4 short address of the Client (this node)
-#define GW_ADDRESS 0x8266    // 802.15.4 short address of the Gateway
+#define GW_ADDRESS 0x5366    // 802.15.4 short address of the Gateway
 
-#define OWN_A_MSB 0x99
-#define msg_n '9'
+#define OWN_A_MSB 0x55
+#define msg_n '5'
